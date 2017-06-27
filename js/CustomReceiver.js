96d47d2e92a543861cc7173824e4e69e9c4dff7b
Prerna Rana <prerna.rana@hsc.com>
2017-06-27 11:45:09 +0530

    s

--- js/CustomReceiver.js
+++ js/CustomReceiver.js
@@ -26,7 +26,7 @@ function CustomReceiver() {
     
     
     messageBus = this.castReceiverManager_.getCastMessageBus(
-    "urn:x-cast:prer.rana.rana",
+    "urn:x-cast:com.google.cast.broadcast",
     cast.receiver.CastMessageBus.MessageType.JSON
 );
 
