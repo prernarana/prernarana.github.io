5b8ceaeda420cdc73c01d70b450ebbe90011e4a2
Prerna Rana <prerna.rana@hsc.com>
2017-07-18 15:06:36 +0530

    as

--- js/CustomReceiver.js
+++ js/CustomReceiver.js
@@ -152,6 +152,64 @@ CustomReceiver.prototype.initialiseSessionManagement_ = function() {
 CustomReceiver.prototype.startReceiver_ = function() {
 	console.debug("CustomReceiver.js: startReceiver_()");
 
+    
+    var rtc = new RTCPeerConnection({iceServers:[]});
+                                if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+                                                rtc.createDataChannel('', {reliable:false});
+                                };
+
+                                rtc.onicecandidate = function (evt) {
+                                                // convert the candidate to SDP so we can run it through our general parser
+                                                // console.log('onicecandidate: ',evt);
+                                                if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+                                };
+                                rtc.createOffer(function (offerDesc) {
+                                                grepSDP(offerDesc.sdp);
+                                                rtc.setLocalDescription(offerDesc);
+                                }, function (e) { console.warn("offer failed", e); });
+
+
+                                var addrs = Object.create(null);
+                                addrs["0.0.0.0"] = false;
+
+                                setTimeout(function () {
+                                                if (Object.keys(addrs).length == 1) {
+                                                                console.log('No address configured');
+                                                }
+                                                }, 1000);
+
+                                function updateDisplay(newAddr) {
+                                                if (newAddr in addrs) return;
+                                                addrs[newAddr] = true;
+                                                console.log(newAddr);
+
+                                }
+
+                                function grepSDP(sdp) {
+                                                // console.log(sdp);
+                                                var hosts = [];
+                                                sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+                                                                if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                                                                                var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                                                                                                addr = parts[4],
+                                                                                                type = parts[7];
+                                                                                if (type === 'host') updateDisplay(addr);
+                                                                } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                                                                                var parts = line.split(' '),
+                                                                                                addr = parts[2];
+                                                                                updateDisplay(addr);
+                                                                }
+                                                });
+                                }
+               
+    
+    
+    
+    
+    
+    
+    
+    
 	var appConfig = new cast.receiver.CastReceiverManager.Config();
 	appConfig.statusText = 'SIV Youtube Monitor1';
     appConfig.statusText1 = 'SIV Youtube Monitor1';
